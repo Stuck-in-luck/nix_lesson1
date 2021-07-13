ab7f948de71245df214381ec101fe9420ca12498
Stuck-in-luck <sinya.kyrylo@gmail.com>
2021-07-13 19:53:36 +0300

    Homework 5 added (Patterns)

--- src/com/alevel/homework4/SofaBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.alevel.homework4;
-
-public interface SofaBuilder {
-   void build();
-   void setPillows(int pillows);
-   void setColor(String color);
-
-}
--- src/com/alevel/homework4/SofaDirector.java
+++
@@ -1,4 +0,0 @@
-package com.alevel.homework4;
-
-public class SofaDirector {
-}
